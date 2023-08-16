dd960fa17df1debad574de81682fa7361c784219
AnnaShp <nyutka_slutskaya@mail.ru>
2023-08-16 19:28:10 +0300

    working script for 3 sliders

--- script.js
+++ script.js
@@ -1,67 +1,102 @@
 //BEFORE-AFTER
 // обьявление переменных для блока ДО/ПОСЛЕ
-const slider = document.querySelector('.before_after__slider').children;
-// console.log(slider);
-let sliderMove = document.querySelector('.before_after__slider');
-// console.log(sliderMove);
-const arrSliders = Array.from(slider);
-console.log(arrSliders);
+const sliderBody = document.querySelector('.before_after__slider');
 
-// переменная слайдера не активна
+//1е фото до/после
+const slider1 = document.querySelector('.slider_1');
+const before1 = slider1.querySelector('.before_1');
+const beforeImg1 = before1.querySelector('img');
+const change1 = slider1.querySelector('.change1');
+//2е фото до/после
+const slider2 = document.querySelector('.slider_2');
+const before2 = slider2.querySelector('.before_2');
+const beforeImg2 = before2.querySelector('img');
+const change2 = slider2.querySelector('.change2');
+//3е фото до/после
+const slider3 = document.querySelector('.slider_3');
+const before3 = slider3.querySelector('.before_3');
+const beforeImg3 = before3.querySelector('img');
+const change3 = slider3.querySelector('.change3');
+
 let isActive = false;
-let width;
 
-// при загрузке страницы ширина картинки равна 100% блоку слайдера
+// расположение фото на всю ширину слайдера при загрузке страницы
 document.addEventListener('DOMContentLoaded', () => {
-    width = arrSliders.offsetWidth;
-    // beforeImg.style.width = `${width}px`;
+    let width1 = slider1.offsetWidth;
+    beforeImg1.style.width = `${width1}px`;
+
+    let width2 = slider2.offsetWidth;
+    beforeImg2.style.width = `${width2}px`;
+
+    let width3 = slider3.offsetWidth;
+    beforeImg3.style.width = `${width3}px`;
 });
 
+// перемещение вертикальной линии 1 слайдер
+function beforeAfterSlider1(x) {
+    let shift1 = Math.max(0, Math.min(x, slider1.offsetWidth));
+    before1.style.width = `${shift1}px`;
+    change1.style.left = `${shift1}px`;
+}
+// перемещение вертикальной линии 2 слайдер
+function beforeAfterSlider2(x) {
+    let shift2 = Math.max(0, Math.min(x, slider2.offsetWidth));
+    before2.style.width = `${shift2}px`;
+    change2.style.left = `${shift2}px`;
+}
+// перемещение вертикальной линии 3 слайдер
+function beforeAfterSlider3(x) {
+    let shift3 = Math.max(0, Math.min(x, slider3.offsetWidth));
+    before3.style.width = `${shift3}px`;
+    change3.style.left = `${shift3}px`;
+}
+// функция остановки вертикальной линии
+function pause(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    return false;
+}
+// слушатель на 1й слайдер при движении мыши
+slider1.addEventListener('mousemove', (e) => {
+    if (!isActive) {
+        return;
+    }
+    let x = e.pageX;
+    x -= slider1.getBoundingClientRect().left;
 
-arrSliders.forEach((el) => {
-    let before = el.querySelector('.slider_photo_before');
-    let beforeImg = before.querySelector('img');
-    let change = el.querySelector('.slider_change');
-
-    beforeImg.style.width = `${width}px`;
-
-    // функция сдвига слайдера
-    function beforeAfterSlider(x) {
-        let shift = Math.max(0, Math.min(x, el.offsetWidth));
-        before.style.width = `${shift}px`;
-        change.style.left = `${shift}px`;
-    };
-
-    // функция остановки
-    function pauseEventsSlider(e) {
-        e.stopPropagation();
-        e.preventDefault();
-        return false;
-    };
-
-    // слушатель события при движении мыши по блоку до/после
-    sliderMove.addEventListener('mousemove', (e) => {
-        if (!isActive) {
-            return;
-        }
-
-        let x = e.pageX;
-        x -= el.getBoundingClientRect().left;
-        beforeAfterSlider(x);
-        pauseEventsSlider(e);
-    });
+    beforeAfterSlider1(x);
+    pause(e);
 });
+// слушатель на 2й слайдер при движении мыши
+slider2.addEventListener('mousemove', (e) => {
+    if (!isActive) {
+        return;
+    }
+    let x = e.pageX;
+    x -= slider2.getBoundingClientRect().left;
 
+    beforeAfterSlider2(x);
+    pause(e);
+});
+// слушатель на 3й слайдер при движении мыши
+slider3.addEventListener('mousemove', (e) => {
+    if (!isActive) {
+        return;
+    }
+    let x = e.pageX;
+    x -= slider3.getBoundingClientRect().left;
 
-sliderMove.addEventListener('mousedown', () => {
+    beforeAfterSlider3(x);
+    pause(e);
+});
+// слушатель на нажатие/отжатиеи на 'уход' мыши из блока
+sliderBody.addEventListener('mousedown', () => {
     isActive = true;
-})
-
-sliderMove.addEventListener('mouseup', () => {
+});
+sliderBody.addEventListener('mouseup', () => {
     isActive = false;
 });
-
-sliderMove.addEventListener('mouseleave', () => {
+sliderBody.addEventListener('mouseleave', () => {
     isActive = false;
 });
 
