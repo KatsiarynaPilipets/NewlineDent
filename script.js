ea04674dfcb14453af8dfecbcf0306d229ae6485
DinaraDervel <dinaradervel@gmail.com>
2023-08-23 09:03:14 -0600

    add adaptive

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const videoIframe = document.getElementById('video-iframe');
 
     openButton.addEventListener('click', function () {
-        var videoId = '3be_6eG4E9w'; // Замените на фактический идентификатор видео, указанный в URL видео после "v="
+        const videoId = '3be_6eG4E9w'; // Замените на фактический идентификатор видео, указанный в URL видео после "v="
         videoIframe.src = 'https://www.youtube.com/embed/' + videoId;
         videoModal.style.display = 'block';
     });
