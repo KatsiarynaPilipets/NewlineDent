c12f07a2373b1bbe346e15c42bf0828caca4f228
Elena763 <elena.evg@bk.ru>
2023-08-24 20:31:50 +0300

    Update footer. Update fonts. Added button up

---
+++ script.js
@@ -0,0 +1,12 @@
+// Button "Up"
+window.addEventListener('scroll', function ()
+{
+    let scroll = document.querySelector('.upward');
+    scroll.classList.toggle("active", window.scrollY>500)
+})
+function scrollTopTop() {
+    window.scrollTo({
+        top:0,
+        behavior: 'smooth'
+    })
+}
