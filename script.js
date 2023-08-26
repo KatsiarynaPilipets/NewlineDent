7541c994aa9582b6b80d719cb762cd97787a56a8
Elena763 <elena.evg@bk.ru>
2023-08-26 22:41:19 +0300

    Added modals

--- script.js
+++ script.js
@@ -10,3 +10,37 @@ function scrollTopTop() {
         behavior: 'smooth'
     })
 }
+ // Получаем элементы модального окна и изображения
+ const modalLicense = document.getElementById("license-modal");
+ const modalImageLicense = document.getElementById("license-modal__image");
+
+ // Отображаем модальное окно и устанавливаем источник изображения
+ function displayModal(img)
+ {
+    modalLicense.style.display = "block";
+    modalImageLicense.src = img.src;
+ }
+
+ // Скрываем содержимое модального окна, если пользователь кликнул вне его
+ function hideModalLicense()
+ {
+     if (event.target == modalLicense) {
+        modalLicense.style.display = "none";
+     }
+ }
+// Получаем элементы модального окна и изображения
+ const modalWarranty = document.getElementById("warranty-modal");
+ const modalImageWarranty = document.getElementById("warranty-modal__image");
+
+ function displayWarranty(img)
+ {
+    modalWarranty.style.display = "block";
+    modalImageWarranty.src = img.src;
+ }
+
+function hideModalWarranty()
+ {
+     if (event.target == modalWarranty) {
+        modalWarranty.style.display = "none";
+     }
+ }
