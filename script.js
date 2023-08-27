77c82f5c7afc1ebb7878a1bc6f8b62479c7fad06
kotenochek27 <katrinya1@gmail.com>
2023-08-27 01:56:12 -0400

    add reviews

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Первые два блока
 document.addEventListener('DOMContentLoaded', function () {
     const callButton = document.getElementById('callButton');
     callButton.addEventListener('click', function () {
@@ -28,3 +29,34 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+//Отзывы
+
+const openButton = document.getElementById('open-button');
+const closeButton = document.getElementById('close-button');
+const videoItems = document.getElementById('video-section')
+const videoModal = document.getElementById('video-modal');
+const videoIframe = document.getElementById('video-iframe');
+
+videoItems.forEach(function(item) {
+   const video = item.querySelector('video');
+   video.addEventListener('click', function() {
+   });
+ });
+
+openButton.addEventListener('click', function () {
+    const videoId = 'fkse3SatzAM'; // Замените на фактический идентификатор видео, указанный в URL видео после "v="
+    videoIframe.src = 'https://www.youtube.com/embed/' + videoId;
+    videoModal.style.display = 'block';
+});
+
+closeButton.addEventListener('click', function () {
+    videoIframe.src = '';
+    videoModal.style.display = 'none';
+});
+
+videoModal.addEventListener('click', function (event) {
+    if (event.target === videoModal) {
+        videoIframe.src = '';
+        videoModal.style.display = 'none';
+    }
+});
