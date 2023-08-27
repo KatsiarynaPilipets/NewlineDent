d997ea0b3096028b0e0576ae8a72f94620249bb0
kotenochek27 <katrinya1@gmail.com>
2023-08-27 13:59:31 -0400

    add wheel

--- script.js
+++ script.js
@@ -29,34 +29,60 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-//Отзывы
+//Фортуна
 
-const openButton = document.getElementById('open-button');
-const closeButton = document.getElementById('close-button');
-const videoItems = document.getElementById('video-section')
-const videoModal = document.getElementById('video-modal');
-const videoIframe = document.getElementById('video-iframe');
+document.addEventListener('DOMContentLoaded', function() {
+    const banner = document.querySelector('.banner');
+    const overlay = document.querySelector('.overlay');
+    const submitBtn = document.getElementById('submitBtn');
+    const closeButton = document.getElementById('close-button');
+    const bannerIframe = document.getElementById('banner-iframe');
 
-videoItems.forEach(function(item) {
-   const video = item.querySelector('video');
-   video.addEventListener('click', function() {
-   });
- });
+    const showBanner = function() {
+      banner.style.display = 'block';
+      overlay.style.display = 'block';
+    };
 
-openButton.addEventListener('click', function () {
-    const videoId = 'fkse3SatzAM'; // Замените на фактический идентификатор видео, указанный в URL видео после "v="
-    videoIframe.src = 'https://www.youtube.com/embed/' + videoId;
-    videoModal.style.display = 'block';
-});
+    // Показывать баннер и затемнение каждые 5 секунд
+    setInterval(showBanner, 5000);
 
-closeButton.addEventListener('click', function () {
-    videoIframe.src = '';
-    videoModal.style.display = 'none';
-});
+    // Закрывать баннер и затемнение при клике вне баннера
+    overlay.addEventListener('click', function() {
+      banner.style.display = 'none';
+      overlay.style.display = 'none';
+    });
 
-videoModal.addEventListener('click', function (event) {
-    if (event.target === videoModal) {
-        videoIframe.src = '';
-        videoModal.style.display = 'none';
-    }
-});
+
+    closeButton.addEventListener('click', function () {
+      banner.style.display = 'none';
+      overlay.style.display = 'none';
+  });
+
+  const phoneInputButton = document.querySelector('.phone-input-button');
+    phoneInputButton.addEventListener('click', function() {
+      const phoneInput = document.querySelector('.phone-input-input');
+      const phoneNumber = phoneInput.value;
+    });
+
+  });
+
+  const wheel = document.querySelector('.wheel');
+  const resultInput = document.getElementById('result');
+
+  function spinWheel() {
+    const randomDegrees = Math.floor(Math.random() * 6) + 1;
+
+    wheel.style.animationName = `wheel-rotate-${randomDegrees}`;
+    resultInput.value = getResultMessage(randomDegrees);
+  }
+
+  function updateDate() {
+    const dateElement = document.getElementById('date');
+    const currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + 1);
+    const options = { day: 'numeric', month: 'long'};
+    const formattedDate = currentDate.toLocaleDateString('ru-RU', options);
+    dateElement.textContent = formattedDate;
+  }
+
+  setInterval(updateDate, 100);
