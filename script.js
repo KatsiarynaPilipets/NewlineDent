54d6ff68f6525a10c49e6b105436b18069747451
kotenochek27 <katrinya1@gmail.com>
2023-08-30 20:48:58 -0400

    add styles wheel

--- script.js
+++ script.js
@@ -34,9 +34,8 @@ document.addEventListener('DOMContentLoaded', function () {
 document.addEventListener('DOMContentLoaded', function() {
     const banner = document.querySelector('.banner');
     const overlay = document.querySelector('.overlay');
-    const submitBtn = document.getElementById('submitBtn');
     const closeButton = document.getElementById('close-button');
-    const bannerIframe = document.getElementById('banner-iframe');
+
 
     const showBanner = function() {
       banner.style.display = 'block';
@@ -64,17 +63,46 @@ document.addEventListener('DOMContentLoaded', function() {
       const phoneNumber = phoneInput.value;
     });
 
-  });
-
   const wheel = document.querySelector('.wheel');
+  const arrow = document.getElementById('arrow');
   const resultInput = document.getElementById('result');
 
   function spinWheel() {
     const randomDegrees = Math.floor(Math.random() * 6) + 1;
 
     wheel.style.animationName = `wheel-rotate-${randomDegrees}`;
-    resultInput.value = getResultMessage(randomDegrees);
+    wheel.style.display = "block";
+
+    switch (randomDegrees) {
+      case 1:
+        resultInput.value = "Бесплатная 3D диагностика зубов";
+        break;
+      case 2:
+        resultInput.value = "Портативный ирригатор в подарок";
+        break;
+      case 3:
+        resultInput.value = "Скидка 25 000 рублей на имплантацию";
+        break;
+      case 4:
+        resultInput.value = "Комплекс по уходу за полостью рта";
+        break;
+      case 5:
+        resultInput.value = "Консультация стоматолога бесплатно";
+        break;
+      case 6:
+        resultInput.value = "Имплантация All-on-4 в подарок";
+        break;
+      default:
+        alert("Нет таких значений");
+    }
+
+    setTimeout(function() {
+      resultInput.value = getResultMessage(randomDegrees);
+    }, 3000);
+
   }
+
+    arrow.addEventListener('click', spinWheel, {once:true});
 
   function updateDate() {
     const dateElement = document.getElementById('date');
@@ -86,3 +114,23 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   setInterval(updateDate, 100);
+
+  function getResultMessage(randomDegrees) {
+    switch (randomDegrees) {
+      case 1:
+        return "Бесплатная 3D диагностика зубов";
+      case 2:
+        return "Портативный ирригатор в подарок";
+      case 3:
+        return "Скидка 25 000 рублей на имплантацию";
+      case 4:
+        return "Комплекс по уходу за полостью рта";
+      case 5:
+        return "Консультация стоматолога бесплатно";
+      case 6:
+        return "Имплантация All-on-4 в подарок";
+      default:
+        return "Нет таких значений";
+    }
+  }
+});
