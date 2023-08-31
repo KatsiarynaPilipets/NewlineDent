21f8dea1464622330bf2ab1c20d967b8da9a01c3
valeryshel <valeryshel@gmail.com>
2023-08-31 23:39:11 +0300

    fix

--- quiz_popup.js
+++ quiz_popup.js
@@ -1,139 +1,122 @@
-
 // Фон попап окна
-let popupBg = document.querySelector('.container__quiz');
+let popupBg = document.querySelector(".container__quiz");
 // Само окно
-let popup = document.querySelector('.quiz1');
-let popupPages = document.querySelectorAll('.quiz');
+let popup = document.querySelector(".quiz1");
+let popupPages = document.querySelectorAll(".quiz");
 let arrPages = Array.from(popupPages);
 // console.log(arrPages);
 
 // Кнопка открытия модального окна(из блока turquoise)
-let openPopupButton = document.querySelector('.btn_sign_white-blue2');
+let openPopupButton = document.querySelector(".btn_sign_white-blue2");
 
 // Все кнопки 'далее'
-const allButtonNext = document.querySelectorAll('.btn_gray_next');
+const allButtonNext = document.querySelectorAll(".btn_gray_next");
 const arrButtonNext = Array.from(allButtonNext);
 // console.log(arrButtonNext);
 
 // Все кнопки 'вернуться назад'
-let allBackButtons = document.querySelectorAll('.btn_gray_back');
+let allBackButtons = document.querySelectorAll(".btn_gray_back");
 const arrButtonBack = Array.from(allBackButtons);
 // console.log(arrButtonBack);
 
 // слушаем кнопку 'заявка на онлайн-консультацию'
-openPopupButton.addEventListener('click', (e) => {
-    e.preventDefault(); // Предотвращаем дефолтное поведение браузера
-    popupBg.classList.add('active'); // Добавляем класс 'active' для фона
-    popup.classList.add('active'); // И для самого окна
-    nextButton(); //кнопки вперед
-    backButton(); //кнопки назад
-    checkForm();
+openPopupButton.addEventListener("click", (e) => {
+  e.preventDefault(); // Предотвращаем дефолтное поведение браузера
+  popupBg.classList.add("active"); // Добавляем класс 'active' для фона
+  popup.classList.add("active"); // И для самого окна
+  nextButton(); //кнопки вперед
+  backButton(); //кнопки назад
+  checkForm();
 });
 
 // функция листания кнопок вперед
 function nextButton() {
-    arrButtonNext.forEach(button => {
-        button.addEventListener('click', (e) => {
-            e.preventDefault(); // Предотвращаем дефолтное поведение браузера
-            //ищем индекс кнопки в массиве
-            let indexButton = arrButtonNext.indexOf(button);
-            // console.log(indexButton);
-            arrPages.forEach(page => {
-                if ((indexButton >= 0) && (indexButton < 5)) {
-                    // индекс следующей страницы будет на 1 больше
-                    indexNextPage = indexButton + 1;
-                    // console.log(indexNextPage);
-                    page.classList.remove('active');
-                    arrPages[indexNextPage].classList.add('active');
-                }
-                else if (indexButton <= arrButtonNext.length) {
-                    // убираем класс active со страницы и модального окна
-                    page.classList.remove('active');
-                    popupBg.classList.remove('active');
-                }
-            });
-        });
+  arrButtonNext.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      e.preventDefault(); // Предотвращаем дефолтное поведение браузера
+      //ищем индекс кнопки в массиве
+      let indexButton = arrButtonNext.indexOf(button);
+      // console.log(indexButton);
+      arrPages.forEach((page) => {
+        if (indexButton >= 0 && indexButton < 5) {
+          // индекс следующей страницы будет на 1 больше
+          indexNextPage = indexButton + 1;
+          // console.log(indexNextPage);
+          page.classList.remove("active");
+          arrPages[indexNextPage].classList.add("active");
+        } else if (indexButton <= arrButtonNext.length) {
+          // убираем класс active со страницы и модального окна
+          page.classList.remove("active");
+          popupBg.classList.remove("active");
+        }
+      });
     });
-};
+  });
+}
 
 // функция листания кнопок назад
 function backButton() {
-    arrButtonBack.forEach(button => {
-        button.addEventListener('click', (e) => {
-            e.preventDefault();// Предотвращаем дефолтное поведение браузера
-            let indexButton = arrButtonBack.indexOf(button);
-            arrPages.forEach(page => {
-                let indexPrevPage = arrPages.indexOf(page);
-                if (indexButton <= arrButtonBack.length) {
-                    indexPrevPage = indexButton;
-                    page.classList.remove('active');
-                    arrPages[indexPrevPage].classList.add('active');
-                }
-                else {
-                    popupBg.classList.remove('active')
-                }
-            });
-        });
+  arrButtonBack.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      e.preventDefault(); // Предотвращаем дефолтное поведение браузера
+      let indexButton = arrButtonBack.indexOf(button);
+      arrPages.forEach((page) => {
+        let indexPrevPage = arrPages.indexOf(page);
+        if (indexButton <= arrButtonBack.length) {
+          indexPrevPage = indexButton;
+          page.classList.remove("active");
+          arrPages[indexPrevPage].classList.add("active");
+        } else {
+          popupBg.classList.remove("active");
+        }
+      });
     });
-};
-
-document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
-    if (e.target === popupBg) { // Если цель клика - фон, то:
-        popupBg.classList.remove('active'); // Убираем активный класс с фона
-    }
-});
-
-
-// РАБОЧИЕ РАДИОКНОПКИ: МЕНЯЮТ ФОН, ВЫВОДЯТ ЗНАЧЕНИЯ В КОНСОЛЬ
-let arrRadio = Array.from(document.querySelectorAll('.radio'));
-arrRadio.forEach(radio => radio.addEventListener('change', () => {
-    for (let radio of arrRadio) {
-        if (radio.checked) {
-            radio.parentElement.classList.add('active');
-            // console.log(radio.parentNode);
-            checkForm();
-
-        } else if (!radio.checked) {
-            radio.parentElement.classList.remove('active');
-        }
-    }
-}));
-
-let arrForms = Array.from(document.forms);
-function checkForm() {
-    // слушаем каждую форму на странице
-    arrForms.forEach(form => form.addEventListener('change', () => {
-        // console.log(form);
-        let arrelements = Array.from(form.elements.radio); //собираем все радиокнопки в массив
-        // console.log(arrelements);
-        let formData = new FormData(form); //создаем фордату
-        for (let elem of arrelements) {
-            let radioValue = elem.value;
-            let radioQ = form.querySelector('.quiz__title').textContent;
-
-            if (elem.checked) {
-                formData.append(radioQ, radioValue);
-                // formData.set(radioQ, radioValue);
-                // Список пар ключ/значение
-                for (let [radioQ, radioValue] of formData) {
-                    console.log(`${radioQ}:${radioValue}`);
-                }
-            }
-        }
-        console.log(formData);
-    }))
+  });
 }
 
+document.addEventListener("click", (e) => {
+  // Вешаем обработчик на весь документ
+  if (e.target === popupBg && e.target !== popupPages) {
+    // Если цель клика - фон, то:
+    popupBg.classList.remove("active"); // Убираем активный класс с фона
+  }
+});
 
+// РАБОЧИЕ РАДИОКНОПКИ: МЕНЯЮТ ФОН, ВЫВОДЯТ ЗНАЧЕНИЯ В КОНСОЛЬ
+let arrRadio = Array.from(document.querySelectorAll(".radio"));
+arrRadio.forEach((radio) =>
+  radio.addEventListener("change", () => {
+    for (let radio of arrRadio) {
+      if (radio.checked) {
+        radio.parentElement.classList.add("active");
+        checkForm();
+      } else if (!radio.checked) {
+        radio.parentElement.classList.remove("active");
+      }
+    }
+  })
+);
 
+const quizForm = document.getElementById("quiz");
 
+let formData = new FormData(quizForm); //создаем фордату
+function checkForm() {
+  // слушаем каждую форму на странице
 
+  quizForm.addEventListener("change", () => {
+    let arrelements = Array.from(quizForm.elements.radio); //собираем все радиокнопки в массив
+    console.log(arrelements);
+    for (let elem of arrelements) {
+      let radioValue = elem.value;
+      let radioQ = quizForm.querySelector(".quiz__title").textContent; // !!! все равно будет брать только первое значение, подумать, как передавать заголовки
 
-
-
-
-
-
-
-
-
+      if (elem.checked) {
+        formData.append(radioQ, radioValue);
+        // Список пар ключ/значение
+        for (let [radioQ, radioValue] of formData) {
+          console.log(`${radioQ}:${radioValue}`);
+        }
+      }
+    }
+  });
+}
